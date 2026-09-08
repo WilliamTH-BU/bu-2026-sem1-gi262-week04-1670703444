8ce5fec1f9762be69b74adc6eb6d40e17566630c
WilliamTH-BU <william.smit@bumail.net>
2026-09-08 11:02:25 +0700

    Workshop

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment
 {
@@ -8,9 +9,9 @@ namespace Assignment
         public void Start()
         {
             // LCT01_SyntaxList();
-            // LCT02_SyntaxLinkedList();
+            //LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+            LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,7 +23,41 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            // [Node 1]
+            linkedlist.AddLast("Node1");
+            // [Node 1] [Node 2]
+            linkedlist.AddLast("Node2");
+            // [Node 0] [Node 1] [Node 2]
+            linkedlist.AddFirst("Node0");
+
+            LinkedListNode<string> firstNode = linkedlist.First;
+            Debug.Log("first: " +  firstNode.Value);
+
+            LinkedListNode<string> lastNode = linkedlist.Last;
+            Debug.Log("last: " +  lastNode.Value);
+
+            Debug.Log("firstNode.Next: " + firstNode.Next.Value);
+            Debug.Log("firstNode.Next.Next: " + firstNode.Next.Next.Value);
+
+            Debug.Log("lastNode.Previous: " + lastNode.Previous.Value);
+            Debug.Log("lastNode.Previous.Previous: " + lastNode.Previous.Previous.Value);
+            Debug.Log("lastNode.Previous.Previous.Previous: " + lastNode.Previous.Previous.Previous.Value);
+
+            if (firstNode.Previous.Value == null) Debug.Log("firstNode.Previous == null");
+            if (lastNode.Next.Value == null) Debug.Log("lastNode.Next.Value == null");
+
+            linkedlist.AddAfter(firstNode, "Node 0.5");
+            linkedlist.AddBefore(lastNode, "Node 1.5");
+
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+
+            linkedlist.Remove("Node 1");
+            linkedlist.Remove(node1);
+            linkedlist.RemoveLast();
+            linkedlist.RemoveFirst();
+
+            linkedlist.Clear();
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,9 +67,52 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
-        }
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+            var inv2 = new Dictionary<string, int>();
 
+            // "Potion": 1
+            inv.Add("Potion", 1);
+
+            // "Potion": 1
+            // "Apple": 10
+            inv.Add("Apple", 10);
+
+            // "Potion": 1
+            // "Apple": 10
+            // "Banana": 5
+            inv["Banana"] = 5;
+
+            // "Potion": 5
+            // "Apple": 10
+            // "Banana": 5
+            inv["Potion"] = 5;
+
+            var pickupItem = "Sword";
+            inv[pickupItem] = 1;
+
+            foreach (KeyValuePair<string, int> pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value; {value}");
+            }
+
+            bool appleExits = inv.ContainsKey("Apple");
+            Debug.Log(appleExits);
+
+            bool keyExits = inv.ContainsKey("Key");
+            Debug.Log(appleExits);
+
+            inv.Remove("Apple");
+            foreach (KeyValuePair<string, int> pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value; {value}");
+            }
+
+
+        }
         #endregion
     }
 }
--- Assets/Workshop/Student/Scripts/Inventory.cs
+++ Assets/Workshop/Student/Scripts/Inventory.cs
@@ -10,26 +10,50 @@ namespace Solution {
         public void AddItem(string item, int amount)
         {
             // 1. ตรวจสอบว่ามีไอเท็มนี้ในคลังแล้วหรือยัง
-           
+            if (inventory.ContainsKey(item))
+            {
+                inventory[item] += amount;
+            }
+            else
+            {
+                inventory.Add(item, amount);
+            }
 
             Debug.Log("Added " + amount + " " + item + ". Total: " + inventory[item]);
+            PrintInventory();
         }
 
         // ลบไอเท็ม
         public void RemoveItem(string item, int amount)
         {
             //4. ตรวจสอบว่ามีไอเท็มนี้ในคลังหรือไม่
-            
+            if (HasItem(item, amount))
+            {
+                inventory[item] -= amount;
+                if (inventory[item] <= 0)
+                {
+                    inventory.Remove(item);
+                }
+            }
+
         }
         public bool HasItem(string item, int amount)
         {
             //2. ตรวจสอบว่ามีไอเท็มนี้ในคลังหรือไม่ และมีจำนวนเพียงพอหรือไม่
+            if (inventory.ContainsKey(item))
+            {
+                return inventory[item] >= amount;
+            }
             return false;
         }
         // ตรวจสอบจำนวนไอเท็ม
         public int GetItemCount(string item)
         {
             //3. ตรวจสอบว่ามีไอเท็มนี้ในคลังหรือไม่ ถ้ามีให้คืนค่าจำนวนไอเท็มนั้น
+            if (inventory.ContainsKey(item))
+            {
+                return inventory[item];
+            }
             return 0;
         }
 
